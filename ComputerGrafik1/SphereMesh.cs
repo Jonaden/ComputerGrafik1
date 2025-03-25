5be0af619f96c4db1f0fc14470a1c3afa096966b
Jonaden <jonatan-gamer@live.dk>
2025-03-25 11:24:31 +0100

    SphereMesh be working

--- ComputerGrafik1/SphereMesh.cs
+++ ComputerGrafik1/SphereMesh.cs
@@ -12,80 +12,73 @@ namespace ComputerGrafik1
     {
 
         float radius = 1;
-        int sectorCount = 2;
-        int stackCount = 3;
+        int sectorCount = 16;
+        int stackCount = 8;
 
-
-        private List<float> listofVertices = new List<float>();
-        private List<uint> listofIndices = new List<uint>();
+        private static int elementBufferObject;
+        private static int vertexArrayObject;
+        private static int vertexBufferObject;
+        private static bool buffersCreated;
+        
         private List<float> texCoords = new List<float>();
         private List<float> listOfNormals = new List<float>();
 
-        public SphereMesh(float radius, int sectorCount, int stackCount)
-        {
-            this.radius = radius;
-            this.sectorCount = sectorCount;
-            this.stackCount = stackCount;
-            
-        }
-
         
-        protected override uint[] Indices => base.Indices;
-        protected override float[] Vertices => new float[]
-         {
-            -0.5f, -0.5f, 0.0f, //Bottom-left vertex
-            0.5f, -0.5f, 0.0f, //Bottom-right vertex
-            0.0f,  0.5f, 0.0f  //Top vertex
-        };
-        static int vertexArrayObject;
-        static int vertexBufferObject;
-        private static bool buffersCreated;
 
-        public void GenerateVertices()
+        float x, y, z, xy;
+        float nx, ny, nz;
+        float s, t;
+
+        public List<float> GenerateVertices()
         {
             float lengthInv = 1.0f / radius;
             float sectorStep = 2 * MathF.PI / sectorCount;
             float stackStep = MathF.PI / stackCount;
             float sectorAngle, stackAngle;
-            
+
+
+            List<float> listofVertices = new List<float>();
 
             for(int i = 0; i <= stackCount; i++)
             {
                 stackAngle = MathF.PI / 2 - i * stackStep;
-                float xy = radius * MathF.Cosh(stackAngle);
-                float z = radius * MathF.Sinh(stackAngle);
+                float xy = radius * MathF.Cos(stackAngle);
+                float z = radius * MathF.Sin(stackAngle);
 
                 for(int j = 0; j <= sectorCount; j++)
                 {
                     sectorAngle = j * sectorStep;
 
-                    float x = xy * MathF.Cosh(sectorAngle);
-                    float y = xy * MathF.Sinh(sectorAngle);
+                    x = xy * MathF.Cos(sectorAngle);
+                    y = xy * MathF.Sin(sectorAngle);
 
                     listofVertices.Add(x);
                     listofVertices.Add(y);
                     listofVertices.Add(z);
 
-                    float nx = x * lengthInv;
-                    float ny = y * lengthInv;
-                    float nz = z * lengthInv;
+                    nx = x * lengthInv;
+                    ny = y * lengthInv;
+                    nz = z * lengthInv;
 
                     listOfNormals.Add(nx);
                     listOfNormals.Add(ny);
                     listOfNormals.Add(nz);
 
-                    float s = (float)j / sectorCount;
-                    float t = (float)i / stackCount;
-                    texCoords.Add(s);
-                    texCoords.Add(t);                   
-                }              
+                    s = (float)j / sectorCount;
+                    t = (float)i / stackCount;
+                    listofVertices.Add(s);
+                    listofVertices.Add(t);          
+                            
+                }  
+                
             }
+            return listofVertices;
         }
 
-        public void GenerateIndices()
+        public List<uint> GenerateIndices()
         {
-            List<int> indices = new List<int>();
-            List<int> lineIndices = new List<int>();
+   
+            List<int> indices = new List<int>();         
             int k1, k2;
 
             for(int i = 0; i < stackCount; i++)
@@ -102,53 +95,57 @@ namespace ComputerGrafik1
                         indices.Add(k1 + 1);
                     }
 
-                    if(i != (stackCount - 1))
+                    if(i != (stackCount-1))
                     {
                         indices.Add(k1 + 1);
                         indices.Add(k2);
-                        indices.Add(k2 + k1);
-                    }
-
-                    lineIndices.Add(k1);
-                    lineIndices.Add(k2);
-
-                    if(i != 0)
-                    {
-                        lineIndices.Add(k1);
-                        lineIndices.Add(k1 + 1);
+                        indices.Add(k2 + 1);
                     }
                 }
             }
-       
+            List<uint> uints = indices.Select(i => (uint)i).ToList();
+
+            return uints;
         }
         protected override void GenerateBuffers()
         {
+
+            Vertices = GenerateVertices().ToArray();
+            Indices = GenerateIndices().ToArray();
 
             if (buffersCreated)
             {
                 return;
             }
+
             vertexBufferObject = GL.GenBuffer();
 
-  
 
             GL.BindBuffer(BufferTarget.ArrayBuffer, vertexBufferObject);          
-            GL.BufferData(BufferTarget.ArrayBuffer, listofVertices.Count * sizeof(float), listofVertices.ToArray(), BufferUsageHint.StaticDraw);
+            GL.BufferData(BufferTarget.ArrayBuffer, Vertices.Length * sizeof(float), Vertices, BufferUsageHint.StaticDraw);
             vertexArrayObject = GL.GenVertexArray();
             GL.BindVertexArray(vertexArrayObject);
-            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 5 * sizeof(float), 0);
+            GL.VertexAttribPointer(1, 2, VertexAttribPointerType.Float, false, 5 * sizeof(float), 3 * sizeof(float));
 
-       
             // Enable variable 0 in the shader.
             GL.EnableVertexAttribArray(0);
+            GL.EnableVertexAttribArray(1);
+
+            elementBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, (int)(elementBufferObject));
+            GL.BufferData(BufferTarget.ElementArrayBuffer, Indices.Length * sizeof(uint), Indices, BufferUsageHint.StaticDraw);
+
             buffersCreated = true;
 
 
         }
         public override void Draw()
         {
-            GL.BindVertexArray(vertexArrayObject);
-            GL.DrawElements(PrimitiveType.Triangles, listofVertices.Count, DrawElementsType.UnsignedInt, listofIndices.Count);
+           //GL.PointSize(10);
+           //GL.DrawArrays(PrimitiveType.Points, 0, Vertices.Length / 5);
+           GL.BindVertexArray(vertexArrayObject);
+           GL.DrawElements(PrimitiveType.Triangles, Indices.Length, DrawElementsType.UnsignedInt, 0);
         }
     }
 }
